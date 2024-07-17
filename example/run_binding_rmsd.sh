a39d191c753b761846e1cd83a0d28a3e52081ed8
Jaemin Sim <78335868+eightmm@users.noreply.github.com>
2024-07-17 14:40:47 +0900

    Update run_binding_rmsd.sh

--- example/run_binding_rmsd.sh
+++ example/run_binding_rmsd.sh
@@ -2,4 +2,4 @@
 python_path="/home/sim/anaconda3/envs/cu12.1/bin/python"
 inference_py="../inference.py"
 
-$python_path $inference_py -r ./1KLT.pdb -l ligands.sdf -o result.csv --model_path ../save 
+$python_path $inference_py -r ./1KLT.pdb -l ./ligands.sdf -o ./result.csv --model_path ../save 
